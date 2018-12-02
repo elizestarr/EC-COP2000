83cf4a8f5cfddf5093b2f3be4fc416249f19736b
elizestarr <45536301+elizestarr@users.noreply.github.com>
2018-12-02 09:11:55 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,75 +1,69 @@
-// Elize Starr Homework 2
-// COP2000
+//Elize Starr COP2000 HW3
+
+
 #include <iostream>
-#include <iomanip>
+#include <cmath>
+
 using namespace std;
 
+void calcInt(float);
 
 int main ()
 {
-    // Constants
-    const int square = 1;
-    const int rectangular = 2;
-    const int round = 3;
-    const int quit = 4;
-    int choice = 0;
-    int width = 0;
-    int length = 0;
-    int radius = 0;
-    const double PI = 3.14;
-    int area = 0;
     
-    // Giving the choices to the user here asking for an input of the choice.
-    cout << "Haverly’s Room Calculator\n";
+    const float GOLD = .01;
+    const float SILVER = .02;
+    const float BRONZE = .04;
+    
+    
+    int rate = 0;
+    int membership_level = 0;
+    
+    cout << "Welcome to Ronda’s Strikeforce Gym!! \n";
     cout << "************************\n";
-    cout << "1. Square Room\n";
-    cout << "2. Rectangular Room\n";
-    cout << "3. Round Room\n";
+    cout << "Membership Fee Calculato";
+    cout << "1. Gold \n";
+    cout << "2. Silver\n";
+    cout << "3. Bronze\n";
     cout << "4. Quit\n";
-    cout << "Please enter a menu item (1-4) \n";
-    cin >> choice;
+    cout << "Please enter a your membership level (1-3 Enter 4 to Quit) \n";
+    cin >> membership_level;
     
-    cout << fixed << showpoint << setprecision (2);
     
-    // if else if statement for the choices picked
-    if (choice == square)
+    switch (membership_level)
     {
-        cout << "Please enter the width of the room\n";
-        cin >> width;
-        cout << "Please enter the length of the room\n";
-        cin>> length;
-        area = length * width;
-        cout << "The area of the Square Room is " << area << endl;
+        case 1:
+            calcInt (GOLD);
+            break;
+        case 2:
+            calcInt (SILVER);
+            break;
+        case 3:
+            calcInt (BRONZE);
+            break;
+        case 4:
+            cout << "Quitting...goodbye";
+            break;
+        default:
+            cout << "Invalid choice";
+            break;
     }
-    // if the user chose a rectangular room
-    else if (choice == rectangular)
+}
+
+void calcInt(float rate)
+{
+    const int base_rate_formula = 1200;
+    const int power_formula = 10;
+    int years_wanted = 1;
+    int result = 0;
+    
+    for (years_wanted; years_wanted <= 10; years_wanted++)
     {
-        cout << "What is the length and width of the room?\n";
-        cin >> length >> width;
-        area= length * width;
-        cout << "The area of the Rectangular Room is " << area << endl;
-    }
-    // if the user chose a round room
-    else if (choice == round)
-    {
-        cout << "What is the radius of the Round Room?\n";
-        cin >> radius;
-        area = (PI * radius * radius);
-        cout << "The area of the Round Room is " << area << endl;
-    }
-    // if the user chose quit
-    else if (choice == quit)
-    {
-        cout<< " Thank you for using Haverly's Room Calculator\n";
+        
+        result = pow((1 + rate), power_formula) * base_rate_formula;
+        cout << "Year " << years_wanted << ": " << result << endl;
     }
     
-    //ensuring that the user only puts 1,2,3, or 4.
-    if (choice > 4)
-        cout << " Invalid integer for choice... error\n";
     
-    if (choice < 1)
-        cout << " Invalid integer for your choice... error\n";
-    
-    return 0;
-    
-    }
+}
+
